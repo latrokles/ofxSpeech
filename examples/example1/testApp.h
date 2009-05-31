15c452dbf7154a73cb8490148bc3461021544b05
latrokles <latrokles@gmail.com>
2009-05-30 20:42:56 -0400

    Wrote some documentation.
    - Some documentation written, very basic... can be better.
    
    - Will test deployment and add more examples.
    
    --L.

--- examples/example1/testApp.h
+++ examples/example1/testApp.h
@@ -6,18 +6,18 @@
 
 class testApp : public ofBaseApp{
 
-	public:
+    public:
 
-		void setup();
-		void update();
-		void draw();
+        void setup();
+        void update();
+        void draw();
 
-		void keyPressed  (int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void resized(int w, int h);
+        void keyPressed  (int key);
+        void mouseMoved(int x, int y );
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void resized(int w, int h);
         
         //-- test event handler for speech recognition, 
         void speechRecognized(string & wordRecognized);
